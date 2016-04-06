e4ce95cbc35a622828fa37bbed732441dc903020
root <mmuehlehner@student.tgm.ac.at>
2016-04-06 18:00:34 +0200

    Added some comments

--- client/src/cb/client.java
+++ client/src/cb/client.java
@@ -27,21 +27,21 @@ public class client extends CallBackPOA
    	{
 	  try
 	  {
-		ORB orb = ORB.init(args,null);
-		org.omg.CORBA.Object o = orb.resolve_initial_references("NameService");
-		NamingContextExt rootContext = NamingContextExtHelper.narrow(o);
-		NameComponent[] name = new NameComponent[2];
+		ORB orb = ORB.init(args,null);//initialise the ORB
+		org.omg.CORBA.Object o = orb.resolve_initial_references("NameService"); //get the reference of the nameservice
+		NamingContextExt rootContext = NamingContextExtHelper.narrow(o); //narrow to correct type
+		NameComponent[] name = new NameComponent[2]; //the name the object is bound to on the server
 		name[0] = new NameComponent("test","my_context");
 		name[1] = new NameComponent("Echo", "Object");
-		Server server = ServerHelper.narrow(rootContext.resolve(name));
+		Server server = ServerHelper.narrow(rootContext.resolve(name)); //search the object on the server
 		client c=new client();
-		POA root_poa = POAHelper.narrow(orb.resolve_initial_references ("RootPOA"));
-		root_poa.the_POAManager().activate();
-		CallBack cb = CallBackHelper.narrow(root_poa.servant_to_reference(c));
+		POA root_poa = POAHelper.narrow(orb.resolve_initial_references ("RootPOA")); //get the root poa
+		root_poa.the_POAManager().activate(); //activate the root poa
+		CallBack cb = CallBackHelper.narrow(root_poa.servant_to_reference(c)); //get the reference of the client object
 		System.out.println("Calling method one_time on server...");
-		server.one_time(cb,"Hello!");
+		server.one_time(cb,"Hello!"); //execute the one_time method on the server
 		System.out.println("Calling methode _cxx_register on server...");
-		server.register(cb,"Hey!",(short)1);
+		server.register(cb,"Hey!",(short)1); //execute the register method on the server
 		while(true); //hält den Client am Leben (keine gute Lösung, nur als Test)
 	  }
 	  catch(Exception e)
